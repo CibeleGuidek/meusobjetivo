678c8232f1692f0a9cf1874e35286a79f02a2848
Cibele Caroline de Oliveira Guidek <cibele.guidek@escola.pr.gov.br>
2024-04-24 22:45:46 +0000

    eucliqueiemsaibamais

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const botoes = document.querySelectorAll('.botao');
+
+  botoes = document.querySelectorAll('.botao');
+const textos = c//******
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function() {
       for(let k=0; k < botoes.length; k++) {
         botoes[k].classList.remove('ativo');
+        textos[k].classList.remove('ativo');//******
       }
     botoes[i].classList.add('ativo');
+    textos[i].classList.add('ativo');//******
   }
 }
+
+//Iniciar os contadores
+const contadores = document.querySelectorAll('.contador');
+
+let dataHoje = new Date();
+
+contadores[0].textContent(dataHoje);
